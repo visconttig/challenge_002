c8f3350c18fba88bc2fdc14093f13fdc77395fb1
Geronimo Viscontti <visconttig@gmail.com>
2025-04-28 23:54:34 -0400

    Add base code for HTTP requests.

--- src/main/java/CurrencyConverter.java
+++ src/main/java/CurrencyConverter.java
@@ -1,2 +1,48 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class CurrencyConverter {
+    String base_code = "USD";
+    String target_code = "CLP";
+    double conversion_rate = 00.00;
+
+    String EXCHANGE_API_KEY = System.getenv("EXCHANGE_API_KEY");
+
+
+
+       public CurrencyConverter(){
+           //
+       }
+
+
+
+       public void getHttpValue(){
+            try {
+
+                URI url = new URI("https://v6.exchangerate-api.com/v6/" +
+                                          EXCHANGE_API_KEY +
+                                          "/pair" +
+                                          "/" + base_code +
+                                          "/" + target_code);
+
+                HttpClient client = HttpClient.newBuilder()
+                        .build();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(url).build();
+                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+                Main.printMessage(String.format("Body: %s.%n", response.body()));
+            } catch (IOException | InterruptedException e){
+                Main.printMessage(String.format("An error ocurred: %s.%n", e));
+            } catch (URISyntaxException | NullPointerException e){
+                Main.printMessage(String.format("There was an error: %s.%n", e));
+            } finally {
+                // close
+            }
+       }
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,16 +5,17 @@ public class Main {
     public static void main(String[] args){
         int chosenOption = 0;
         int amountToConvert = 0;
+        CurrencyConverter cConverter = new CurrencyConverter();
 
         try (Scanner sc = new Scanner(System.in)) {
             do {
                 chosenOption = showMenu(sc);
                 printMessage(String.format("\t\tChosen option: %d.%n", chosenOption));
-                // *** call to convertion logic ***
                 printMessage("Enter the amount to convert: ");
                 amountToConvert = askNumber(sc);
                 printMessage(String.format("\t\tAmount to convert: %d.%n", amountToConvert));
                 // ---> Convert
+                cConverter.getHttpValue(); // for testing
                 // Show currency valuation
             } while (chosenOption != 7);
         } catch (IOException e){
